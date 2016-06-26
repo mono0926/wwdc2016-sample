7df8186b6465ce32eb85ccee3f6ace42765750ed
Masayuki Ono <mono0926@gmail.com>
2016-06-26 10:24:11 +0900

    Add initial project

---
+++ wwdc2016-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wwdc2016-sample
+//
+//  Created by OnoMasayuki on 6/26/16.
+//  Copyright © 2016 mono. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ wwdc2016-sampleTests/wwdc2016_sampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  wwdc2016_sampleTests.swift
+//  wwdc2016-sampleTests
+//
+//  Created by OnoMasayuki on 6/26/16.
+//  Copyright © 2016 mono. All rights reserved.
+//
+
+import XCTest
+@testable import wwdc2016_sample
+
+class wwdc2016_sampleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
