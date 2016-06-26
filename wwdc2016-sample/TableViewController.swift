3cccb183467d9a539616c9e5a603d19d863bb613
Masayuki Ono <mono0926@gmail.com>
2016-06-26 10:32:25 +0900

    Change root viewcontroller to table viewcontroller

---
+++ wwdc2016-sample/TableViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ViewController.swift
+//  wwdc2016-sample
+//
+//  Created by OnoMasayuki on 6/26/16.
+//  Copyright © 2016 mono. All rights reserved.
+//
+
+import UIKit
+
+class  TableViewController: UITableViewController {
+}
+
